6fadcdef24d21a838c7c41f6903b158b6914bec4
Yura <loienko.yurii@gmail.com>
2017-05-04 21:17:08 +0300

    1

---
+++ src/net/ukr/dreamsicle/AnketaServlet.java
@@ -0,0 +1,64 @@
+package net.ukr.dreamsicle;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by Yura on 04.05.2017.
+ */
+public class AnketaServlet extends HttpServlet {
+
+    static String Name = "Ann";
+    static final String Surname = "Loienko";
+    static final int Age = 26;
+    public static int q1Yes = 0;
+    public static int q1No = 0;
+    public static int q2Yes = 0;
+    public static int q2No = 0;
+
+    final String TEMPLATE = "<html>" +"<head><title>dreamsicle.ukr.net</title></head>" +
+            "<body><h1>%s</h1></body></html>";
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        String age = request.getParameter("age");
+        String question1 = request.getParameter("question1");
+        String question2 = request.getParameter("question2");
+
+        if (question1.equals("yes"))
+            q1Yes++;
+        if (question1.equals("no"))
+            q1No++;
+
+        if (question2.equals("yes"))
+            q2Yes++;
+        if (question2.equals("no"))
+            q2No++;
+
+        HttpSession session = request.getSession(true);
+        session.setAttribute("user", name+" "+ surname + " " + age);
+
+        /*String result = "<p> Question 1: Yes = " + q1Yes + ", No" + q1No;
+        result += "<p> Question 2: Yes = " + q2Yes + ", No" + q2No;
+
+        response.getWriter().println(String.format(TEMPLATE, result));*/
+        response.sendRedirect("index.jsp");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String a = request.getParameter("a");
+        HttpSession session = request.getSession(false);
+
+        if ("exit".equals(a) && (session != null))
+            session.removeAttribute("user");
+
+        response.sendRedirect("index.jsp");
+    }
+
+}
--- web/index.jsp
+++ web/index.jsp
@@ -1,34 +1,49 @@
-<%--
+<%@ page import="net.ukr.dreamsicle.AnketaServlet" %>
+<%@ page import="net.ukr.dreamsicle.AnketaServlet" %><%--
   Created by IntelliJ IDEA.
-  User: Yura
-  Date: 01.05.2017
-  Time: 20:17
+  User: Yurii
+  Date: 04.05.2017
+  Time: 22:20
   To change this template use File | Settings | File Templates.
 --%>
-
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>net.ukr.dreamsicle</title>
-  </head>
-  <body>
-  <form action="/question" method="POST">
-    Name: <input type="text" name="name">
-    Surname: <input type="text" name="surname">
-    Age: <input type="text" name="age">
+<head>
+    <title>$Title$</title>
+</head>
+<body>
+<% String name = (String)session.getAttribute("user");%>
 
-    <br/>Do you like Java?
-    <br/> <input type="radio" name="question1" value="Yes" />
-    <br/> <input type="radio" name="question1" value="No" />
 
-    <br/>Do you like .Net?
-    <br/> <input type="radio" name="question2" value="Yes" />
-    <br/> <input type="radio" name="question2" value="No" />
+<% if (name == null) { %>
+<form action="/profile" method="POST">
+
+    Name: <input type="text" name="name" /><br/>
+    Surname: <input type="text" name="surname" /><br/>
+    Age: <input type="text" name="age" /><br/>
+
+    <br/> Do you like Trips?
+    <br/><input type="radio" name="question1" value="yes" />   Yes
+    <br/> <input type="radio" name="question1" value="no" />   No
+
+    <br> Do you like London city?<br/>
+    <br/> <input type="radio" name="question2" value="yes" />   Yes
+    <br/> <input type="radio" name="question2" value="no" />    No
 
     <br/><input type="submit" />
-  </form>>
 
-
-
-  </body>
+</form>
+<% } else { %>
+<h1> Hello,  -  <%= name %>  </h1>
+<br>Number of people who like Trips: <b><%=AnketaServlet.q1Yes %></b>
+<br>
+<br>Number of people who don`t like Trips:<b> <%=AnketaServlet.q1No %> </b>
+<br>
+<br>Number of people who like Trips:<b> <%=AnketaServlet.q2Yes %></b>
+<br>
+<br>Number of people who like Trips:<b> <%=AnketaServlet.q2No %></b>
+<br>
+<br>Click this link to <a href="/profile?a=exit">logout</a>
+<% } %>
+</body>
 </html>
